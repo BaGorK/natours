58f710f5d4494d723f17b8d377517261f9fd03f3
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-01 22:32:26 +0300

    feat: set env variable config | add developer logging middleware

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 
-app.get("/test", (req, res) => {
-  res
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.get("/api/v1/test", (req, res) => {
+  return res
     .status(200)
     .json({ message: "Hello from the server side", app: "Natours" });
 });
