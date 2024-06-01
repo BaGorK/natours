e5a055ca4676e9b4f029e9620d8528492dfbc73f
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-01 22:43:47 +0300

    feat: connect to db

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-
 import app from "./app.js";
+import mongoose from "mongoose";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`App running on port ${PORT}...`));
+try {
+  await mongoose.connect(process.env.DB);
+  app.listen(PORT, () =>
+    console.log(`DB connected & App running on port ${PORT}...`)
+  );
+} catch (error) {
+  console.log("Something went wrong");
+  process.exit(1);
+}
