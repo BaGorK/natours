c25ac0ce1a384df671fb31cbdf16d7bc1574e55b
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-01 22:53:10 +0300

    feat: routes | create a user routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 
+import userRouter from "./routes/userRoutes.js";
+
 const app = express();
 
 if (process.env.NODE_ENV === "development") {
@@ -12,5 +14,7 @@ app.get("/api/v1/test", (req, res) => {
     .status(200)
     .json({ message: "Hello from the server side", app: "Natours" });
 });
+
+app.use("/api/v1/users", userRouter);
 
 export default app;
