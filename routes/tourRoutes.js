5557573e2a99f0e0b235da20f697d0e126a9a8f1
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-09 23:29:17 +0300

    feat: create an aliasTopTours route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,8 @@ import * as tourController from "../controllers/tourController.js";
 
 const router = Router();
 
+router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
+
 router
   .route("/")
   .get(tourController.getAllTours)
