ba54e3420065681cb70d00ff5289d5dbbad7edca
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-09 23:15:39 +0300

    refactor: sorting and limiting fields | api features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -12,14 +12,35 @@ class APIFeatures {
     excludedFields.forEach((el) => delete queryObj[el]);
 
     // Advanced Filtering
-    // /api/v1/tours?difficulty=easy&duration[gte]=5
-    // console.log(req.query); // {difficulty: easy, duration: {gte: 5}} // only the $ is missing
-    // {difficulty: easy, duration: {$gte: 5}} // this is the query in mongodb to work with operators
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     console.log(JSON.parse(queryStr));
 
-    this.query = this.query.find(JSON.parse(queryStr)); // this will return query so that we can chain other methods
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt');
+    }
+
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      // /tours?fields=name,duration,price
+      // query = query.select('name duration price');  // select only these field names | aka = projecting
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+
     return this;
   }
 }
