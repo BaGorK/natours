26f669183fb2cf542302fa92b1c4380d013a3277
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-11 03:39:13 +0300

    feat: tourModel | document middleware | pre save hook

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import slugify from 'slugify';
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -64,6 +66,16 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc);
+//   next()
+// })
 
 const Tour = mongoose.model('Tour', tourSchema);
 
