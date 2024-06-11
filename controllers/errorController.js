f2ed6487ef9f2f2a86b812546e349827a3312395
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-11 10:31:23 +0300

    feat: send error for prod and dev

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,28 @@
-const globalErrorHandlerMiddleWare = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
 
+const sendErrorProd = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
   });
+};
+
+const globalErrorHandlerMiddleWare = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
 
 export default globalErrorHandlerMiddleWare;
