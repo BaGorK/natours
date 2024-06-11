e52139136cb3d92c9fabf342a157074ecc84316f
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-11 06:36:24 +0300

    feat: create a global error handler middleware

--- app.js
+++ app.js
@@ -1,38 +1,54 @@
-import express from "express";
-import morgan from "morgan";
+import express from 'express';
+import morgan from 'morgan';
 
-import userRouter from "./routes/userRoutes.js";
-import tourRouter from "./routes/tourRoutes.js";
+import userRouter from './routes/userRoutes.js';
+import tourRouter from './routes/tourRoutes.js';
 
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import path from "path";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, `/public`)));
 
-app.get("/api/v1/test", (req, res) => {
+app.get('/api/v1/test', (req, res) => {
   return res
     .status(200)
-    .json({ message: "Hello from the server side", app: "Natours" });
+    .json({ message: 'Hello from the server side', app: 'Natours' });
 });
 
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/tours", tourRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/tours', tourRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`
-  })
-})
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`
+  // })
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 export default app;
