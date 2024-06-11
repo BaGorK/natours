de6567d18be5aab5856f58306ac962d0dae6e983
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-11 13:24:15 +0300

    feat: create token when signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,16 @@
 import User from '../models/userModel.js';
 import catchAsync from '../utils/catchAsync.js';
+import { createJWT } from '../utils/tokenUtils.js';
 
 export const signup = catchAsync(async (req, res) => {
   const { name, email, password, passwordConfirm } = req.body;
   const newUser = await User.create({ name, email, password, passwordConfirm });
 
+  const token = createJWT({ id: newUser._id });
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
