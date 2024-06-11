741d132e501f362bff34210c389b71e2620f44e9
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-11 16:04:42 +0300

    feat: send bearer token with header | basic validation

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, `/public`)));
 
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  // console.log(req.headers);
+  next();
+});
+
 app.get('/api/v1/test', (req, res) => {
   return res
     .status(200)
