123a3e9ae6db7b9c5954fab40e063bf4ae9ae708
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-14 11:31:37 +0300

    feat: data sanitization | mongo sanitize | xss

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import morgan from 'morgan';
 import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
+import xss from 'xss-clean';
 
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
@@ -38,6 +40,12 @@ app.use(
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 // Serving static files
 app.use(express.static(path.resolve(__dirname, `/public`)));
