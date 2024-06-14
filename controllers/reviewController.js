18ce667209f06bea317a6913dd9f8260ae548a18
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-14 15:25:20 +0300

    feat: create a review controller | get all reviews | get review

---
+++ controllers/reviewController.js
@@ -0,0 +1,26 @@
+import catchAsync from '../utils/catchAsync.js';
+import Review from './../models/reviewModel.js';
+
+export const getAllReviews = catchAsync(async (req, res, next) => {
+  const reviews = await Review.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: reviews.length,
+    data: {
+      reviews,
+    },
+  });
+});
+
+export const createReview = catchAsync(async (req, res, next) => {
+  const review = await Review.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    message: 'review created successfully',
+    data: {
+      review,
+    },
+  });
+});
