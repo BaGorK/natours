eb16e8a9e4862b659f550f8b663c0ddd8659d4ad
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-15 12:41:32 +0300

    feat: add a static method on the review schema to calc avg rating

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -49,6 +49,28 @@ reviewSchema.pre(/^find/, function (next) {
   next();
 });
 
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  console.log(stats);
+};
+
+reviewSchema.post('save', function () {
+  // this points to the current review
+  this.constructor.calcAverageRatings(this.tour); // Review => this.constructor
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 export default Review;
