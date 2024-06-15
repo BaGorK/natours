23affd881eb19611aca28be3af767a8f7f860411
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-15 09:28:03 +0300

    feat: add authentication and authorization | user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,25 +11,23 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-);
+// Protect all routes after this middleware
+router.use(authController.protect);
 
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
-
+router.patch('/updateMyPassword', authController.updatePassword);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
 router.get(
-  '/getMe',
-  authController.protect,
+  '/me',
   userController.getMe,
   userController.getUser
 );
 
+router.use(authController.restrictTo('admin'));
+
 router
   .route('/')
-  .get(authController.protect, userController.getAllUsers)
+  .get(userController.getAllUsers)
   .post(userController.createUser);
 
 router
