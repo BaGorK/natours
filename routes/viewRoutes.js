0ddcca3b3b56db315be20c51d8ce2ca6e27ccd95
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-10-15 09:53:48 +0300

    feat: building the tour details page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,6 +4,6 @@ import { viewsController } from '../controllers/viewsController.js';
 const router = express.Router();
 
 router.get('/', viewsController.getOverview);
-router.get('/tour', viewsController.getTour);
+router.get('/tour/:slug', viewsController.getTour);
 
 export default router;
