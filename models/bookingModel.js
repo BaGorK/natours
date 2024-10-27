98a1fe1b1c6a3d9401430b4c78f422038999d411
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-10-27 16:41:50 +0300

    feat: modelling the bookings

---
+++ models/bookingModel.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const bookingSchema = new mongoose.Schema({
+  tour: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Tour',
+    required: [true, 'Booking must belong to a Tour!'],
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'Booking must belong to a User!'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Booking must have a price!'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  // if the want to book a tour
+  // if the user has no account num and wants to pay by hand
+  paid: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+bookingSchema.pre(/^find/, function (next) {
+  this.populate('user').populate({
+    path: 'tour',
+    select: 'name',
+  });
+});
+
+const Booking = mongoose.model('Booking', bookingSchema);
+export default Booking;
