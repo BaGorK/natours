eb9f80163b39d06d930292e3c061c0bf90b1cf66
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-10-27 17:38:00 +0300

    feat: create a new route | my-tours

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -14,5 +14,6 @@ router.get(
 router.get('/tour/:slug', viewsController.isLoggedIn, viewsController.getTour);
 router.get('/login', viewsController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
+router.get('/my-tours', authController.protect, viewsController.getMyTours);
 
 export default router;
